bef0092eb4e81d5cd44363807e8342fe8cafb7c9
astridx <astrid.guenther@dimna.de>
2015-12-04 19:30:04 +0100

    working on the access rights

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,31 +1,31 @@
 -- Here we save the actual session. If there are two different sessions, here are two different entries. If there is no session, the table is empty.
 CREATE TABLE IF NOT EXISTS `#__joommark_stats` (
-  `session_id_person` varchar(255) NOT NULL, 
-  `nowpage`           varchar(255) NULL, 
-  `lastupdate_time`   int(11) NULL, 
+  `session_id_person` varchar(255) NOT NULL,
+  `nowpage`           varchar(255) NULL,
+  `lastupdate_time`   int(11) NULL,
   `current_name`  varchar(255) NULL,
   PRIMARY KEY (`session_id_person`)
 ) ENGINE=InnoDB CHARACTER SET `utf8`;
 
 -- If the url changed, we take information from #__joommark_stats to this table. For the seconds we count the difference between the actual time and #__joommark_stats.lastupdate_time.
 CREATE TABLE IF NOT EXISTS `#__joommark_serverstats` (
-  `session_id` varchar(50), 
+  `session_id` varchar(50),
   `user_id_person` INT NULL DEFAULT 0,
-  `customer_name`     varchar(255), 
+  `customer_name`     varchar(255),
   `visitdate`         date,
-  `visit_timestamp`   datetime, 
-  `visitedpage`       varchar(255), 
-  `geolocation`       varchar(255), 
-  `ip`                varchar(255), 
-  `browser`           varchar(255), 
+  `visit_timestamp`   datetime,
+  `visitedpage`       varchar(255),
+  `geolocation`       varchar(255),
+  `ip`                varchar(255),
+  `browser`           varchar(255),
   `os`                varchar(255),
   `seconds` INT NOT NULL DEFAULT 0,
   PRIMARY KEY (`session_id` ,  `visitdate` ,  `visitedpage`)
-) ENGINE=InnoDB CHARACTER SET `utf8`; 
+) ENGINE=InnoDB CHARACTER SET `utf8`;
 
 -- perhaps referrals are interesting for a target
 CREATE TABLE IF NOT EXISTS `#__joommark_referral` (
-  `referral` varchar(255) NOT NULL, 
+  `referral` varchar(255) NOT NULL,
   `record_date` date NOT NULL,
   `ip` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB CHARACTER SET `utf8`;
@@ -34,19 +34,21 @@ CREATE TABLE IF NOT EXISTS `#__joommark_referral` (
 CREATE TABLE IF NOT EXISTS `#__joommark_messages` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
+  `catid` int(11) NOT NULL DEFAULT '0',
+  `asset_id` int(11) NOT NULL DEFAULT '0',
   `menuitems`varchar(255) NOT NULL,
   `accesslevel` smallint(6) DEFAULT NULL,
   `published` tinyint(4) NOT NULL,
   `percentage` int(3) unsigned,
-  `message` varchar(255) NOT NULL, 
-  `cookie` mediumint(9) NOT NULL,   
+  `message` varchar(255) NOT NULL,
+  `cookie` mediumint(9) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET `utf8`;
 
 -- perhaps search words are interesting for a target
 CREATE TABLE IF NOT EXISTS `#__joommark_searches` (
   `searchword` varchar(255) NOT NULL,
-  `user_id_person` INT NULL DEFAULT 0, 
+  `user_id_person` INT NULL DEFAULT 0,
   `record_date` date NOT NULL
 ) ENGINE=InnoDB CHARACTER SET `utf8`;
 
@@ -54,11 +56,12 @@ CREATE TABLE IF NOT EXISTS `#__joommark_searches` (
 CREATE TABLE IF NOT EXISTS `#__joommark_plansstats` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `catid` int(11) NOT NULL DEFAULT '0',
+  `asset_id` int(11) NOT NULL DEFAULT '0',
   `state` tinyint(1) NOT NULL default '0',
   `type` varchar(50) NOT NULL,
-  `name` varchar(255) NOT NULL, 
-  `description` text NULL , 
-  `hastarget` tinyint NOT NULL DEFAULT '0', 
+  `name` varchar(255) NOT NULL,
+  `description` text NULL ,
+  `hastarget` tinyint NOT NULL DEFAULT '0',
   `target_expectation` int(11) NULL,
   `checked_out` int(11) UNSIGNED NOT NULL DEFAULT '0',
   `checked_out_time` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -71,10 +74,10 @@ CREATE TABLE IF NOT EXISTS `#__joommark_plansstats` (
 
 -- If the admin wants to be informed, if the plan is reached, we have to track …
 CREATE TABLE IF NOT EXISTS `#__joommark_plansstats_track` (
-  `plan_id` int(11) NOT NULL, 
-  `session_id` varchar(200) NOT NULL, 
-  `plandate` date NOT NULL, 
-  `plan_timestamp` int(11) NOT NULL, 
+  `plan_id` int(11) NOT NULL,
+  `session_id` varchar(200) NOT NULL,
+  `plandate` date NOT NULL,
+  `plan_timestamp` int(11) NOT NULL,
   PRIMARY KEY (`plan_id`, `session_id`, `plandate`)
 ) ENGINE=InnoDB CHARACTER SET `utf8`;
 
@@ -86,3 +89,5 @@ CREATE TABLE IF NOT EXISTS `#__joommark_feedbacksstats` (
   `ordering` int(11) NOT NULL default '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB CHARACTER SET `utf8`;
+
+
--- admin/tables/plans.php
+++ admin/tables/plans.php
@@ -1,24 +1,94 @@
 <?php
 /**
-* @ author Jose A. Luque
-* @ Copyright (c) 2011 - Jose A. Luque
-* @license GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
-*/
-// No direct access
+ * @package     Joomla.Administrator
+ * @subpackage  com_jommark
+ *
+ * @copyright   Copyright (C) 2014-2015 Jose A. Luque and Astrid Günther. All rights reserved.
+ * @license     GNU General Public License version 2
+ */
+
 defined('_JEXEC') or die('Restricted access');
 
 /**
- * Joommark Table class
+ * class Joommark Tabele Plans
+ *
+ * @since  1.0
  */
 class JoommarkTablePlans extends JTable
 {
 	/**
-	 * Constructor
+	 * Object constructor
 	 *
-	 * @param   JDatabaseDriver  &$db  A database connector object
+	 * @param   JDatabaseDriver  $db  JDatabaseDriver object.
+	 *
+	 * @since   1.0
 	 */
-	function __construct(&$db)
+	function __construct($db)
 	{
 		parent::__construct('#__joommark_plansstats', 'id', $db);
+	}
+
+	/**
+	 * Method to return the name
+	 *
+	 * @return  string  The string to use as the title in the asset table.
+	 *
+	 * @since   1.0
+	 */
+	protected function _getAssetName()
+	{
+		$k = $this->_tbl_key;
+
+		return 'com_joommark.plan.' . (int) $this->$k;
+	}
+
+	/**
+	 * Method to return the title to use for the asset table.  In
+	 * tracking the assets a title is kept for each asset so that there is some
+	 * context available in a unified access manager.  Usually this would just
+	 * return $this->title or $this->name or whatever is being used for the
+	 * primary name of the row. If this method is not overridden, the asset name is used.
+	 *
+	 * @return  string  The string to use as the title in the asset table.
+	 *
+	 * @link    https://docs.joomla.org/JTable/getAssetTitle
+	 * @since   1.0
+	 */
+	protected function _getAssetTitle()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * Method to get the parent asset under which to register this one.
+	 * By default, all assets are registered to the ROOT node with ID,
+	 * which will default to 1 if none exists.
+	 * The extended class can define a table and id to lookup.  If the
+	 * asset does not exist it will be created.
+	 *
+	 * @return  integer
+	 *
+	 * @since   1.0
+	 */
+	protected function _getAssetParentId()
+	{
+		$assetParent = JTable::getInstance('asset');
+		$assetParentId = $assetParent->getRootId();
+
+		if (($this->catid) && !empty($this->catid))
+		{
+			$assetParent->loadByName('com_joommark.category.' . (int) $this->catid);
+		}
+		else
+		{
+			$assetParent->loadByName('com_joommark');
+		}
+
+		if ($assetParent->id)
+		{
+			$assetParentId = $assetParent->id;
+		}
+
+		return $assetParentId;
 	}
 }
