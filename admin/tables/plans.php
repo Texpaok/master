dbab2d07b80cc7803502e24165e08fb98970f30f
astridx <astrid.guenther@dimna.de>
2015-12-04 19:56:20 +0100

    the asset of the plans object is OK now

--- admin/tables/plans.php
+++ admin/tables/plans.php
@@ -6,9 +6,7 @@
  * @copyright   Copyright (C) 2014-2015 Jose A. Luque and Astrid Günther. All rights reserved.
  * @license     GNU General Public License version 2
  */
-
 defined('_JEXEC') or die('Restricted access');
-
 /**
  * class Joommark Tabele Plans
  *
@@ -90,5 +88,71 @@ class JoommarkTablePlans extends JTable
 		}
 
 		return $assetParentId;
+	}
+
+	/**
+	 * Method to load a row from the database by primary key and bind the fields
+	 * to the JTable instance properties.
+	 *
+	 * @param   mixed    $array   An optional primary key value to load the row by, or an array of fields to match.  If not
+	 *                            set the instance property value is used.
+	 * @param   boolean  $ignore  True to reset the default values before loading the new row.
+	 *
+	 * @return  boolean  True if successful. False if row not found.
+	 *
+	 * @link    https://docs.joomla.org/JTable/load
+	 * @since   1.0
+	 * @throws  InvalidArgumentException
+	 * @throws  RuntimeException
+	 * @throws  UnexpectedValueException
+	 */
+	public function bind($array, $ignore = '')
+	{
+		if (isset($array['params']) && is_array($array['params']))
+		{
+			$parameter = new JRegistry;
+			$parameter->loadArray($array['params']);
+			$array['params'] = (string) $parameter;
+		}
+
+		if (isset($array['rules']) && is_array($array['rules']))
+		{
+			$rules = new JAccessRules($array['rules']);
+			$this->setRules($rules);
+		}
+
+		return parent::bind($array, $ignore);
+	}
+
+	/**
+	 * Method to load a row from the database by primary key and bind the fields
+	 * to the JTable instance properties.
+	 *
+	 * @param   mixed    $pk     An optional primary key value to load the row by, or an array of fields to match.  If not
+	 *                           set the instance property value is used.
+	 * @param   boolean  $reset  True to reset the default values before loading the new row.
+	 *
+	 * @return  boolean  True if successful. False if row not found.
+	 *
+	 * @link    https://docs.joomla.org/JTable/load
+	 * @since   1.0
+	 * @throws  InvalidArgumentException
+	 * @throws  RuntimeException
+	 * @throws  UnexpectedValueException
+	 */
+	public function load($pk = null, $reset = true)
+	{
+		if (parent::load($pk, $reset))
+		{
+			$params = new JRegistry;
+			$params->loadString($this->params, 'JSON');
+			$this->params = $params;
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
